609a4ca82bb788ae58c8de9f063b1bb423f4a908
Andrew Zheng <andrewzhengyu@gmail.com>
2023-08-22 18:29:57 +0800

    clerk updated

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,8 @@
 import Image from 'next/image'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div>Home page</div>
+    <Button>Button</Button>
   )
 }
